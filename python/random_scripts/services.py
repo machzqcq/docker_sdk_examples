28676d60b50fb54ce40506be13a1fdba52fb3310
machzqcq <pradeep@seleniumframework.com>
2017-02-24 17:02:29 -0500

    1) docker services

--- python/random_scripts/services.py
+++ python/random_scripts/services.py
@@ -9,31 +9,11 @@ Read this for concepts - https://github.com/machzqcq/docker-orchestration
 """
 conf = configparser.RawConfigParser()
 conf.read('../config/.env')
-
-# In case you want to assert the values. Note: If you are using virtualenv, then the below will not locate docker-machine binary
-# node1_ip = subprocess.getoutput("docker-machine ip node1")
-# assert str(node1_ip) in conf.get('docker-machine-node1','DOCKER_HOST')
-# node2_ip = subprocess.getoutput('docker-machine ip node2')
-# assert str(node2_ip) in conf.get('docker-machine-node2','DOCKER_HOST')
-# node3_ip = subprocess.getoutput('docker-machine ip node3')
-# assert str(node3_ip) in conf.get('docker-machine-node3','DOCKER_HOST')
-
 node1_client = MyDockerClient(conf, 'docker-machine-node1').client
 node2_client = MyDockerClient(conf, 'docker-machine-node2').client
 node3_client = MyDockerClient(conf, 'docker-machine-node3').client
 
-# Initialize a swarm and leave it. If the node is already part of swarm, then try, except to handle it properly
-# try:
-#     node1_client.swarm.init(
-#         advertise_addr='eth1', listen_addr='0.0.0.0:2377',
-#         force_new_cluster=False, snapshot_interval=5000,
-#         log_entries_for_slow_followers=1200
-#     )
-# except Exception as err:
-#     print("OS error: {0}".format(err))
-#     print(type(err))
-#     print(err.args)
-#
+
 
 # eth1 instead of eth0 , becuase the 192.x address was assigned by virtual box and that is what docker-machine has
 node1_client.swarm.init(
@@ -52,24 +32,18 @@ node2_client.swarm.join(remote_addrs=["192.168.99.103:2377"],join_token=worker_t
 # Join Node3 as Worker
 node3_client.swarm.join(remote_addrs=["192.168.99.103:2377"], join_token=worker_token,
                         advertise_addr='eth1', listen_addr='0.0.0.0:2377')
-print(node1_client.swarm.attrs)
 
-# List nodes
-for node in node1_client.nodes.list():
-    print("NodeID,Attributes\n")
-    print("{0},{1}".format(node.id, node.attrs))
 
-# List only worker nodes
-
-for node in node1_client.nodes.list(filters={'role': 'worker'}):
-    print("NodeID,Attributes\n")
-    print("{0},{1}".format(node.id, node.attrs))
+nginx_service = node1_client.services.create(image='nginx:alpine',args=["-p 80:80/tcp","--replicas 2"],name="nginx")
+print("ServiceName,Attributes")
+print("{0},{1}".format(nginx_service.name,nginx_service.attrs))
+# nginx_service.remove()
 
 # force=True needed for manager to leave the swarm
-node2_client.swarm.leave()
-node3_client.swarm.leave()
-node1_client.swarm.leave(force=True)
-print(node1_client.swarm.attrs)
+# node2_client.swarm.leave()
+# node3_client.swarm.leave()
+# node1_client.swarm.leave(force=True)
+# print(node1_client.swarm.attrs)
 
 # Note: Bad things can really happen if swarm enters a dangling state. In fact my VM's lost docker-engine state too
 # The docker-machine provisioner got corrupted - hence I had to docker-machine provision node1 (to re-provision)
